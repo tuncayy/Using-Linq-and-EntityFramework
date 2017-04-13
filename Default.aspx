11a348f403c55c6b359277fea8abc46327e72151
Tuncay Yılmaz <tuncayyilmaz5@gmail.com>
2017-04-13 20:50:00 +0300

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem>1</asp:ListItem>
+            <asp:ListItem>2</asp:ListItem>
+            <asp:ListItem>3</asp:ListItem>
+            <asp:ListItem>4</asp:ListItem>
+            <asp:ListItem>5</asp:ListItem>
+            <asp:ListItem>6</asp:ListItem>
+            <asp:ListItem>7</asp:ListItem>
+            <asp:ListItem>8</asp:ListItem>
+            <asp:ListItem>9</asp:ListItem>
+            <asp:ListItem>All</asp:ListItem>
+        </asp:DropDownList>
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged">0</asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged">10000</asp:TextBox>
+    
+        <asp:DropDownList ID="DropDownList2" runat="server">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:DataList ID="DataList1" runat="server">
+            <ItemTemplate>
+                <table style="width:100%;">
+                    <tr>
+                        <td>
+                            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                        </td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+                        </td>
+                        <td>
+                            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+                        </td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                </table>
+            </ItemTemplate>
+        </asp:DataList>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+           using (eShopperDBEntities products = new eShopperDBEntities())
+           {
+               var query = from p in products.tProducts
+                           
+                           select p;
+
+
+               tProduct product = (query.FirstOrDefault<tProduct>());
+               GridView1.DataSource = query.ToList();
+               GridView1.DataBind();
+           }
+        using (eShopperDBEntities categpry = new eShopperDBEntities())
+        {
+            var query =( from c in categpry.tCategories
+
+                        select c );
+
+
+           //   tCategory categpryies = (query.FirstOrDefault<tCategory>());
+            //    GridView1.DataSource = query.ToList();
+            //    GridView1.DataBind();
+           
+           
+            DropDownList2.DataSource = query.ToList();
+            DropDownList2.DataTextField = "CategoryName";
+            DropDownList2.DataValueField = "CategoryID";
+            DropDownList2.DataBind();
+        }
+        
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int min = Convert.ToInt32(TextBox1.Text);
+        int max = Convert.ToInt32(TextBox2.Text);
+        if(DropDownList1.SelectedIndex < 9)
+        { using (eShopperDBEntities products = new eShopperDBEntities())
+        {
+            var query = from p in products.tProducts
+                        where p.CategoryID == DropDownList1.SelectedIndex + 1
+                        where p.Price >= min
+                        where p.Price <= max
+                        
+                        select p;
+
+
+            tProduct product = (query.FirstOrDefault<tProduct>());
+            GridView1.DataSource = query.ToList();
+            GridView1.DataBind();
+        }}
+        else
+        {
+            using (eShopperDBEntities products = new eShopperDBEntities())
+            {
+                var query = from p in products.tProducts
+                          
+                            where p.Price >= min
+                            where p.Price <= max
+                            select p;
+
+
+                tProduct product = (query.FirstOrDefault<tProduct>());
+                GridView1.DataSource = query.ToList();
+                GridView1.DataBind();
+            }
+        }
+        
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int min = Convert.ToInt32(TextBox1.Text);
+        int max = Convert.ToInt32(TextBox2.Text);
+        if (DropDownList1.SelectedIndex < 9)
+        {
+            using (eShopperDBEntities products = new eShopperDBEntities())
+            {
+                var query = from p in products.tProducts
+                            where p.CategoryID == DropDownList1.SelectedIndex + 1
+                            where p.Price >= min
+                            where p.Price <= max
+
+                            select p;
+
+
+                tProduct product = (query.FirstOrDefault<tProduct>());
+                GridView1.DataSource = query.ToList();
+                GridView1.DataBind();
+            }
+        }
+        else
+        {
+            using (eShopperDBEntities products = new eShopperDBEntities())
+            {
+                var query = from p in products.tProducts
+
+                            where p.Price >= min
+                            where p.Price <= max
+                            select p;
+
+
+                tProduct product = (query.FirstOrDefault<tProduct>());
+                GridView1.DataSource = query.ToList();
+                GridView1.DataBind();
+            }
+        }
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        int min = Convert.ToInt32(TextBox1.Text);
+        int max = Convert.ToInt32(TextBox2.Text);
+        if (DropDownList1.SelectedIndex < 9)
+        {
+            using (eShopperDBEntities products = new eShopperDBEntities())
+            {
+                var query = from p in products.tProducts
+                            where p.CategoryID == DropDownList1.SelectedIndex + 1
+                            where p.Price >= min
+                            where p.Price <= max
+
+                            select p;
+
+
+                tProduct product = (query.FirstOrDefault<tProduct>());
+                GridView1.DataSource = query.ToList();
+                GridView1.DataBind();
+            }
+        }
+        else
+        {
+            using (eShopperDBEntities products = new eShopperDBEntities())
+            {
+                var query = from p in products.tProducts
+
+                            where p.Price >= min
+                            where p.Price <= max
+                            select p;
+
+
+                tProduct product = (query.FirstOrDefault<tProduct>());
+                GridView1.DataSource = query.ToList();
+                GridView1.DataBind();
+            }
+        }
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+        int min = Convert.ToInt32(TextBox1.Text);
+        int max = Convert.ToInt32(TextBox2.Text);
+        if (DropDownList1.SelectedIndex < 9)
+        {
+            using (eShopperDBEntities products = new eShopperDBEntities())
+            {
+                var query = from p in products.tProducts
+                            where p.CategoryID == DropDownList1.SelectedIndex + 1
+                            where p.Price >= min
+                            where p.Price <= max
+
+                            select p;
+
+
+                tProduct product = (query.FirstOrDefault<tProduct>());
+                GridView1.DataSource = query.ToList();
+                GridView1.DataBind();
+            }
+        }
+        else
+        {
+            using (eShopperDBEntities products = new eShopperDBEntities())
+            {
+                var query = from p in products.tProducts
+
+                            where p.Price >= min
+                            where p.Price <= max
+                            select p;
+
+
+                tProduct product = (query.FirstOrDefault<tProduct>());
+                GridView1.DataSource = query.ToList();
+                GridView1.DataBind();
+            }
+        }
+    }
+}
